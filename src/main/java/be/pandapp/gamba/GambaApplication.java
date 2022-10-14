fca5ff5e4f32369393bdd10d623fbb4f41c3fcb5
Joachim FRANCESCANGELI <joachim.francescangeli@etnic.be>
2022-10-14 15:48:36 +0200

    init: initial commit

---
+++ src/main/java/be/pandapp/gamba/GambaApplication.java
@@ -0,0 +1,13 @@
+package be.pandapp.gamba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GambaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GambaApplication.class, args);
+	}
+
+}
