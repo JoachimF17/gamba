8f2d73ecc59b8cef2fa113a7cea85c10636405b2
JoachimF17 <joachim.frances@hotmail.be>
2023-11-13 13:28:18 +0100

    feat: add insert betservice impl

---
+++ src/main/java/be/pandapp/gamba/services/bet/BetServiceImpl.java
@@ -0,0 +1,66 @@
+package be.pandapp.gamba.services.bet;
+
+import be.pandapp.gamba.exceptions.IdNotFoundException;
+import be.pandapp.gamba.models.dto.BetDTO;
+import be.pandapp.gamba.models.entities.Bet;
+import be.pandapp.gamba.models.forms.BetForm;
+import be.pandapp.gamba.repos.BetRepository;
+import be.pandapp.gamba.repos.GameRepository;
+import be.pandapp.gamba.repos.PlayerRepository;
+import be.pandapp.gamba.repos.UsersRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BetServiceImpl implements BetService{
+    private final BetRepository betRepository;
+    private final UsersRepository usersRepository;
+    private final PlayerRepository playerRepository;
+    private final GameRepository gameRepository;
+
+    public BetServiceImpl(BetRepository betRepository, UsersRepository usersRepository, PlayerRepository playerRepository, GameRepository gameRepository) {
+        this.betRepository = betRepository;
+        this.usersRepository = usersRepository;
+        this.playerRepository = playerRepository;
+        this.gameRepository = gameRepository;
+    }
+
+    @Override
+    public BetDTO insert(BetForm form) {
+        if(form.getGameId() == null) return null;
+        if(form.getScorerId() == null) return null;
+        if(form.getUsersId() == null) return null;
+
+        Bet bet = form.toBet();
+
+        bet.setScorer(playerRepository.findById(form.getScorerId())
+                .orElseThrow(() -> new IdNotFoundException(form.getScorerId())));
+        bet.setGame(gameRepository.findById(form.getGameId())
+                .orElseThrow(() -> new IdNotFoundException(form.getGameId())));
+        bet.setUsers(usersRepository.findById(form.getUsersId())
+                .orElseThrow(() -> new IdNotFoundException(form.getUsersId())));
+
+        return BetDTO.of(betRepository.save(bet));
+    }
+
+    @Override
+    public BetDTO getOneById(Long id) {
+        return betRepository.findById(id).map(BetDTO::of).orElseThrow(() -> new IdNotFoundException(id));
+    }
+
+    @Override
+    public BetDTO updateById(Long id, BetForm form) {
+        return null;
+    }
+
+    @Override
+    public BetDTO deleteById(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<BetDTO> getAll() {
+        return null;
+    }
+}
